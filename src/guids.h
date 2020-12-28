00632c08a04f39d189d72d5b8ea2bc31cd047ff5
fishca <sergrud@mail.ru>
2020-12-28 20:52:19 +0300

    Добавлен файл GUID-ов метаданных

---
+++ src/guids.h
@@ -0,0 +1,315 @@
+#pragma once
+// GUID-s
+
+constexpr const char* GUID_Subsystems                  = "37f2fa9a-b276-11d4-9435-004095e12fc7";
+constexpr const char* GUID_CommonModules               = "0fe48980-252d-11d6-a3c7-0050bae0a776";
+constexpr const char* GUID_SessionParameters           = "24c43748-c938-45d0-8d14-01424a72b11e";
+constexpr const char* GUID_Roles                       = "09736b02-9cac-4e3f-b4f7-d3e9576ab948";
+constexpr const char* GUID_CommonAttributes            = "15794563-ccec-41f6-a83c-ec5f7b9a5bc1";
+constexpr const char* GUID_ExchangePlans               = "857c4a91-e5f4-4fac-86ec-787626f1c108";
+constexpr const char* GUID_FilterCriteria              = "3e7bfcc0-067d-11d6-a3c7-0050bae0a776";
+constexpr const char* GUID_EventSubscriptions          = "4e828da6-0f44-4b5b-b1c0-a2b3cfe7bdcc";
+constexpr const char* GUID_ScheduledJobs               = "11bdaf85-d5ad-4d91-bb24-aa0eee139052";
+constexpr const char* GUID_FunctionalOptions           = "af547940-3268-434f-a3e7-e47d6d2638c3";
+constexpr const char* GUID_FunctionalOptionsParameters = "30d554db-541e-4f62-8970-a1c6dcfeb2bc";
+constexpr const char* GUID_DefinedTypes                = "c045099e-13b9-4fb6-9d50-fca00202971e";
+constexpr const char* GUID_SettingsStorages            = "46b4cd97-fd13-4eaa-aba2-3bddd7699218";
+constexpr const char* GUID_CommonForms                 = "07ee8426-87f1-11d5-b99c-0050bae0a95d";
+constexpr const char* GUID_CommonCommands              = "2f1a5187-fb0e-4b05-9489-dc5dd6412348";
+constexpr const char* GUID_CommandGroups               = "1c57eabe-7349-44b3-b1de-ebfeab67b47d";
+constexpr const char* GUID_Interfaces                  = "39bddf6a-0c3c-452b-921c-d99cfa1c2f1b";
+constexpr const char* GUID_CommonTemplates             = "0c89c792-16c3-11d5-b96b-0050bae0a95d";
+constexpr const char* GUID_CommonPictures              = "7dcd43d9-aca5-4926-b549-1842e6a4e8cf";
+constexpr const char* GUID_XDTOPackages                = "cc9df798-7c94-4616-97d2-7aa0b7bc515e";
+constexpr const char* GUID_WebServices                 = "8657032e-7740-4e1d-a3ba-5dd6e8afb78f";
+constexpr const char* GUID_HTTPServices                = "0fffc09c-8f4c-47cc-b41c-8d5c5a221d79";
+constexpr const char* GUID_WSReferences                = "d26096fb-7a5d-4df9-af63-47d04771fa9b";
+constexpr const char* GUID_StyleItems                  = "58848766-36ea-4076-8800-e91eb49590d7";
+constexpr const char* GUID_Styles                      = "3e5404af-6ef8-4c73-ad11-91bd2dfac4c8";
+constexpr const char* GUID_Languages                   = "9cd510ce-abfc-11d4-9434-004095e12fc7";
+constexpr const char* GUID_Catalogs                    = "cf4abea6-37b2-11d4-940f-008048da11f9";
+constexpr const char* GUID_Constants                   = "0195e80c-b157-11d4-9435-004095e12fc7";
+constexpr const char* GUID_Documents                   = "061d872a-5787-460e-95ac-ed74ea3a3e84";
+constexpr const char* GUID_Numerators                  = "36a8e346-9aaa-4af9-bdbd-83be3c177977";
+constexpr const char* GUID_Sequences                   = "bc587f20-35d9-11d6-a3c7-0050bae0a776";
+constexpr const char* GUID_JournDocuments              = "4612bd75-71b7-4a5c-8cc5-2b0b65f9fa0d";
+constexpr const char* GUID_Enums                       = "f6a80749-5ad7-400b-8519-39dc5dff2542";
+constexpr const char* GUID_Reports                     = "631b75a0-29e2-11d6-a3c7-0050bae0a776";
+constexpr const char* GUID_DataProcessors              = "bf845118-327b-4682-b5c6-285d2a0eb296";
+constexpr const char* GUID_ChartOfCharacteristicTypes  = "82a1b659-b220-4d94-a9bd-14d757b95a48";
+constexpr const char* GUID_ChartsOfAccounts            = "238e7e88-3c5f-48b2-8a3b-81ebbecb20ed";
+constexpr const char* GUID_ChartsOfCalculationTypes    = "30b100d6-b29f-47ac-aec7-cb8ca8a54767";
+constexpr const char* GUID_InformationRegisters        = "13134201-f60b-11d5-a3c7-0050bae0a776";
+constexpr const char* GUID_AccumulationRegisters       = "b64d9a40-1642-11d6-a3c7-0050bae0a776";
+constexpr const char* GUID_AccountingRegisters         = "2deed9b8-0056-4ffe-a473-c20a6c32a0bc";
+constexpr const char* GUID_CalculationRegisters        = "f2de87a8-64e5-45eb-a22d-b3aedab050e7";
+constexpr const char* GUID_BusinessProcesses           = "fcd3404e-1523-48ce-9bc0-ecdb822684a1";
+constexpr const char* GUID_Tasks                       = "3e63355c-1378-4953-be9b-1deb5fb6bec5";
+constexpr const char* GUID_ExternalDataSources         = "5274d9fc-9c3a-4a71-8f5e-a0db8ab23de5";
+
+constexpr const char* GUID_config   = "30ffe4cc-eef2-4371-8b26-046597e37e22";
+constexpr const char* GUID_lang     = "ae3cd3da-a1c0-4b8e-8edf-4d25e853a54e";
+
+constexpr const char* file_root     = "root";
+constexpr const char* file_version  = "version";
+constexpr const char* file_versions = "versions";
+
+
+constexpr const char* _root_ =
+"{2,30ffe4cc-eef2-4371-8b26-046597e37e22,h55/Mge2fMFztQsFisVlH3XXYBo/afhvXqqsee54Ki3K7GqjefSutU5iFD5GFXc2\n"
+"ANz7GLei9du1H2euYINWQg==}";
+
+constexpr const char* _version_ =
+"{\n"
+"{216,0,\n"
+"{80316,0}\n"
+"}\n"
+"}";
+
+constexpr const char* _versions_ = "{1,6,\"\",b091ac3f-01c3-4eb3-baaa-b35922abaccd,\"30ffe4cc-eef2-4371-8b26-046597e37e22\",76dc5902-3208-497a-8bbb-b57b81c49c25,\"ae3cd3da-a1c0-4b8e-8edf-4d25e853a54e\",181e11fb-0c66-4452-a13c-4421d903566a,\"root\",3e2c7a91-6df6-42be-a959-0e75547a12ce,\"version\",271e22c1-57f1-4d9a-a915-fedaff6e3f2c,\"versions\",2edceba4-96e0-4f37-997e-b2c65ba732ff}";
+
+constexpr const char* _lang_83_ =
+"{1,\n"
+"{0,\n"
+"{2,\n"
+"{1,0,ae3cd3da-a1c0-4b8e-8edf-4d25e853a54e},\"Русский\",\n"
+"{1,\"ru\",\"Русский\"},\"\",0,0,00000000-0000-0000-0000-000000000000},\"ru\" }, 0}";
+
+constexpr const char* _conf_root_83_ =
+"{2,\n"
+"{30ffe4cc-eef2-4371-8b26-046597e37e22},6,\n"
+"{9cd510cd-abfc-11d4-9434-004095e12fc7,\n"
+"{1,\n"
+"{59,\n"
+"{0,\n"
+"{2,\n"
+"{1,0,53a9569f-8670-44b8-b424-dee2dce96e57},\"ИмяКонфигурации\",\n"
+"{1,\"ru\",\"Синоним конфигурации (имя конфигурации)\"},\"\",0,2,7f676314-716a-4d54-8335-a71a3857b21c,3,9dfcabbf-6a7a-48aa-8721-df5e78b367c6,3,00000000-0000-0000-0000-000000000000}\n"
+//"{1,0,53a9569f-8670-44b8-b424-dee2dce96e57},\"BB71D8BD57664101889B95C34FEA2351\",\n"
+//"{1,\"ru\",\"B b71 d8 b d57664101889 b95 c34 FEA2351\"},\"\",0,2,7f676314-716a-4d54-8335-a71a3857b21c,3,9dfcabbf-6a7a-48aa-8721-df5e78b367c6,3,00000000-0000-0000-0000-000000000000}\n"
+"},\"\",1,\n"
+"{0},\n"
+"{0},\n"
+"{0},\n"
+"{0},\n"
+"{0},00000000-0000-0000-0000-000000000000,ae3cd3da-a1c0-4b8e-8edf-4d25e853a54e,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,0,\"\",\"\",\"\",1,\n"
+"{0,0},1,\n"
+"{0,0},1,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,80316,\n"
+"{0,0},0,0,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,\n"
+"{1,\n"
+"{\"#\",1708fdaa-cbce-4289-b373-07a5a74bee91,1}\n"
+"},00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,2,00000000-0000-0000-0000-000000000000,3,\n"
+"{0,0},\n"
+"{24,\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,9},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,1},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,10},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,2},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,3},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,5},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,6},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,7},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,8},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,11},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,12},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,16},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,17},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,18},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,19},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,20},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,21},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,22},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,23},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,24},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,25},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,26},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,27},\n"
+"{\"B\",0}\n"
+"},\n"
+"{\n"
+"{\"#\",e4c53f94-e5f7-4a34-8c10-218bd811cae1,28},\n"
+"{\"B\",1}\n"
+"}\n"
+"},2,\"\",80316,1,0,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000,1,00000000-0000-0000-0000-000000000000,\n"
+"{1,24,\n"
+"{1,0,0},\n"
+"{2,0,0},\n"
+"{3,0,0},\n"
+"{5,0,0},\n"
+"{6,0,0},\n"
+"{7,0,0},\n"
+"{8,0,0},\n"
+"{9,0,0},\n"
+"{10,0,0},\n"
+"{11,0,0},\n"
+"{12,0,0},\n"
+"{16,0,0},\n"
+"{17,0,0},\n"
+"{18,0,0},\n"
+"{19,0,0},\n"
+"{20,0,0},\n"
+"{21,0,0},\n"
+"{22,0,0},\n"
+"{23,0,0},\n"
+"{24,0,0},\n"
+"{25,0,0},\n"
+"{26,0,0},\n"
+"{27,0,0},\n"
+"{28,1,0}\n"
+"},\n"
+"{0,0}\n"
+"},23,\n"
+"{09736b02-9cac-4e3f-b4f7-d3e9576ab948,0},\n"
+"{0c89c792-16c3-11d5-b96b-0050bae0a95d,0},\n"
+"{0fe48980-252d-11d6-a3c7-0050bae0a776,0},\n"
+"{0fffc09c-8f4c-47cc-b41c-8d5c5a221d79,0},\n"
+"{11bdaf85-d5ad-4d91-bb24-aa0eee139052,0},\n"
+"{15794563-ccec-41f6-a83c-ec5f7b9a5bc1,0},\n"
+"{24c43748-c938-45d0-8d14-01424a72b11e,0},\n"
+"{30d554db-541e-4f62-8970-a1c6dcfeb2bc,0},\n"
+"{37f2fa9a-b276-11d4-9435-004095e12fc7,0},\n"
+"{39bddf6a-0c3c-452b-921c-d99cfa1c2f1b,0},\n"
+"{3e5404af-6ef8-4c73-ad11-91bd2dfac4c8,0},\n"
+"{3e7bfcc0-067d-11d6-a3c7-0050bae0a776,0},\n"
+"{46b4cd97-fd13-4eaa-aba2-3bddd7699218,0},\n"
+"{4e828da6-0f44-4b5b-b1c0-a2b3cfe7bdcc,0},\n"
+"{58848766-36ea-4076-8800-e91eb49590d7,0},\n"
+"{7dcd43d9-aca5-4926-b549-1842e6a4e8cf,0},\n"
+"{857c4a91-e5f4-4fac-86ec-787626f1c108,0},\n"
+"{8657032e-7740-4e1d-a3ba-5dd6e8afb78f,0},\n"
+"{9cd510ce-abfc-11d4-9434-004095e12fc7,1,ae3cd3da-a1c0-4b8e-8edf-4d25e853a54e},\n"
+"{af547940-3268-434f-a3e7-e47d6d2638c3,0},\n"
+"{c045099e-13b9-4fb6-9d50-fca00202971e,0},\n"
+"{cc9df798-7c94-4616-97d2-7aa0b7bc515e,0},\n"
+"{d26096fb-7a5d-4df9-af63-47d04771fa9b,0}\n"
+"}\n"
+"},\n"
+"{9fcd25a0-4822-11d4-9414-008048da11f9,\n"
+"{6,\n"
+"{1,\n"
+"{\n"
+"{1,0,df725343-700b-493f-914f-c98ccac10414},00000000-0000-0000-0000-000000000000},15,\n"
+"{0195e80c-b157-11d4-9435-004095e12fc7,0},\n"
+"{061d872a-5787-460e-95ac-ed74ea3a3e84,0},\n"
+"{07ee8426-87f1-11d5-b99c-0050bae0a95d,0},\n"
+"{13134201-f60b-11d5-a3c7-0050bae0a776,0},\n"
+"{1c57eabe-7349-44b3-b1de-ebfeab67b47d,0},\n"
+"{2f1a5187-fb0e-4b05-9489-dc5dd6412348,0},\n"
+"{36a8e346-9aaa-4af9-bdbd-83be3c177977,0},\n"
+"{4612bd75-71b7-4a5c-8cc5-2b0b65f9fa0d,0},\n"
+"{631b75a0-29e2-11d6-a3c7-0050bae0a776,0},\n"
+"{82a1b659-b220-4d94-a9bd-14d757b95a48,0},\n"
+"{b64d9a40-1642-11d6-a3c7-0050bae0a776,0},\n"
+"{bc587f20-35d9-11d6-a3c7-0050bae0a776,0},\n"
+"{bf845118-327b-4682-b5c6-285d2a0eb296,0},\n"
+"{cf4abea6-37b2-11d4-940f-008048da11f9,0},\n"
+"{f6a80749-5ad7-400b-8519-39dc5dff2542,0}\n"   
+"}\n"
+"}\n"
+"},\n"
+"{e3687481-0a87-462c-a166-9f34594f9bba,\n"
+"{1,\n"
+"{0,\n"
+"{1,0,1253d360-e2ba-490e-bdc2-43d2c9025924}\n"
+"},2,\n"
+"{238e7e88-3c5f-48b2-8a3b-81ebbecb20ed,0},\n"
+"{2deed9b8-0056-4ffe-a473-c20a6c32a0bc,0}\n"
+"}\n"
+"},\n"
+"{9de14907-ec23-4a07-96f0-85521cb6b53b,\n"
+"{1,\n"
+"{\n"
+"{1,0,63655d7a-38b0-4a9b-a2b3-ec9d3a076fcc}\n"
+"},2,\n"
+"{30b100d6-b29f-47ac-aec7-cb8ca8a54767,0},\n"
+"{f2de87a8-64e5-45eb-a22d-b3aedab050e7,0}\n"
+"}\n"
+"},\n"
+"{51f2d5d8-ea4d-4064-8892-82951750031e,\n"
+"{1,\n"
+"{0,\n"
+"{1,0,46f0e3c2-0590-44c9-953f-7fce90ecd2ac}\n"
+"},2,\n"
+"{3e63355c-1378-4953-be9b-1deb5fb6bec5,0},\n"
+"{fcd3404e-1523-48ce-9bc0-ecdb822684a1,0}\n"
+"}\n"
+"},\n"
+"{e68182ea-4237-4383-967f-90c1e3370bc7,\n"
+"{1,\n"
+"{\n"
+"{1,0,ab48276d-b9e8-45a9-9e4e-bc8f7bea0e0b}\n"
+"},1,\n"
+"{5274d9fc-9c3a-4a71-8f5e-a0db8ab23de5,0}\n"
+"}\n"
+"},\n"
+"{\n"
+"{0,\"\",\"\"}\n"
+"}\n"
+"}";
